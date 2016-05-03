93bc8889bf19b57bd1523b0b5c41c2bb61c7858d
Nate Murray <nate@natemurray.com>
2016-05-03 15:41:30 -0700

    update angular modules in vendor

--- app/ts/vendor.ts
+++ app/ts/vendor.ts
@@ -7,9 +7,10 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 
 // Angular 2
+import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-import 'angular2/platform/common_dom';
 import '@angular/core';
+import '@angular/common';
 import '@angular/router';
 import '@angular/http';
 
