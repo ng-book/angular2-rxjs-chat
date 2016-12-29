9c7015866806d8cd9c10e59fe4f0cd48f6222d13
Nate Murray <nate@natemurray.com>
2016-12-29 14:26:51 -0600

    ignore typescript specs

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ function makeConfig(options) {
       hotUpdateChunkFilename: "updates/[hash].[id].update.js"
     },
     plugins: [
-      new webpack.IgnorePlugin(/spec\.js$/),
+      new webpack.IgnorePlugin(/spec\.[tj]s$/),
       // new webpack.optimize.CommonsChunkPlugin('core.js'),
       new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
       new webpack.optimize.CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor'] }),
