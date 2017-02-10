8d398d5644637c8d1ed6abc35a93923e36bee02d
Nate Murray <nate@natemurray.com>
2017-02-10 12:12:19 -0600

    we're not using app.spec.ts yet

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,17 +1,17 @@
 // import {MessagesService} from '../app/ts/services/services';
 
-import {Calculator} from 'calculator';
+// import {Calculator} from 'calculator';
 
-describe('Hello', () => {
+// describe('Hello', () => {
 
-  it('should test', () => {
-    // let m = MessagesService;
-  });
+//   it('should test', () => {
+//     // let m = MessagesService;
+//   });
 
-  it('should add', () => {
-    let c = new Calculator();
-    expect(true).toBe(true);
-    expect(c.add(1,2)).toBe(3);
-  });
+//   it('should add', () => {
+//     let c = new Calculator();
+//     expect(true).toBe(true);
+//     expect(c.add(1,2)).toBe(3);
+//   });
 
-});
+// });
